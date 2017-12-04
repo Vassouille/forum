4e304b13ecc8a2812d4753bc37972a03b9833d3e
Vassouille <thibault1.vasseur@gmail.com>
2017-12-05 00:19:42 +0100

    Début de développement du thème

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,16 +2,38 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Theme;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="home")
      */
-    public function helloAction()
+    public function helloAction(Request $request)
     {
-        return $this->render('base.html.twig', array());
+        $theme = new Theme();
+
+        $form = $this->createFormBuilder($theme)
+            ->add('name', TextType::class, array('label' => 'Nom du thème'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $theme = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($theme);
+            $em->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('theme.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/AppBundle/Entity/Theme.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="theme")
+ */
+class Theme
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
