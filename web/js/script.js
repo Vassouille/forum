1002f0659d5fc3e9d6688793740825a4a50df25b
Vassouille <thibault1.vasseur@gmail.com>
2017-12-06 23:49:14 +0100

    Ajout des boutons suppresion et modification pour les modérateurs sur les discussions

--- web/js/script.js
+++ web/js/script.js
@@ -2,4 +2,37 @@ $( document ).ready(function () {
     $('#close').click(function () {
        $(this).closest('.row').hide();
     });
+
+    $('[data-target="#supprimerModal"]').click(function () {
+        selector = $(this);
+        $('#supprimerModal a.btn-danger').click(function () {
+            $.ajax({
+                type: "POST",
+                url: $(selector).attr('data-path'),
+                data: {id: $(selector).attr('data-id')}
+            }).done(function (data) {
+                console.log(data);
+                $(selector).closest('tr').remove();
+                $('#supprimerModal').modal('hide');
+            })
+        });
+
+    });
+
+    $('[data-target="#editModal"]').click(function () {
+        selector = $(this);
+        $('textarea#textarea').val($(this).closest('td').prev().find('.discussion-content').html());
+        $('#editModal a.btn-primary').click(function () {
+            $.ajax({
+                type: "POST",
+                url: $(selector).attr('data-path'),
+                data: {id: $(selector).attr('data-id'), content: $('#editModal textarea').val()}
+            }).done(function (data) {
+                console.log(data);
+                $(selector).closest('td').prev().find('.discussion-content').html($('#editModal textarea').val());
+                $('#editModal').modal('hide');
+            })
+        });
+
+    });
 });
